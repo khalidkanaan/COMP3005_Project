feca1277f16799ea3e6273fb821faa403be332a1
Mohamed Abdalla <78317393+Mohman556@users.noreply.github.com>
2021-12-19 19:44:03 -0500

    Update Functions.sql

--- src/SQL/Functions.sql
+++ src/SQL/Functions.sql
@@ -1,3 +1,6 @@
+/*
+Function that checks if a books stock falls below the minimum threshold
+*/
 CREATE OR REPLACE FUNCTION check_stock_amount(book_isbn numeric(13,0))
     RETURNS BOOLEAN AS $$
 BEGIN
@@ -10,6 +13,11 @@ END;
 $$ 	LANGUAGE plpgsql;
 --
 
+/*
+When registering, the user inputs their billing address. At checkout they are asked if they would like to add an aditional address to be used as shipping address.
+A new address is linked to the user and they checkout. If the user goes through checkout once more and picks the different address option, instead of a new address being created,
+the previous shipping address would be updated.
+*/
 CREATE OR REPLACE FUNCTION check_two_addresses(id_user varchar(20))
     RETURNS BOOLEAN AS $$
 BEGIN
@@ -22,6 +30,9 @@ END;
 $$ 	LANGUAGE plpgsql;
 --
 
+/*
+Function used when overwriting the schema. 
+*/
 CREATE OR REPLACE FUNCTION check_schema_exists()
     RETURNS BOOLEAN AS $$
 BEGIN
